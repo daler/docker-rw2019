cb4637a2815892038f4827b00c4b220c86fe399c
Ryan Dale <dalerr@niddk.nih.gov>
2019-05-14 09:58:40 -0400

    add script and template for converting to presentation

---
+++ prep.py
@@ -0,0 +1,5 @@
+with open('template.html') as template, open('README.md') as readme, open('presentation.html', 'w') as pres:
+    pres.write(
+        template.read()
+        .replace('PLACEHOLDERPLACEHOLDERPLACEHOLDER', readme.read())
+    )
