8381b7ed7a65714a06b3adf8d4cca90c99a2006c
Ryan Dale <dalerr@niddk.nih.gov>
2019-05-14 21:45:46 -0400

    switch demo order

--- demos/demo1/run.bash
+++ demos/demo1/run.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd /data
-fastq-dump -X 100 --gzip SRR1574251
-fastqc /data/*.fastq.gz
-Rscript demo.R > R.log
+
+# Download some FASTQ data using fastq-dump from sra-tools:
+docker run \
+  -v $(pwd):/data \
+  quay.io/biocontainers/sra-tools:2.9.1_1--h470a237_0 \
+  bash -c "cd /data && fastq-dump -X 100 --gzip SRR1574251"
+
+# Run FastQC on the resulting file
+docker run \
+  -v $(pwd):/data \
+  quay.io/biocontainers/fastqc:0.11.8--1 \
+  bash -c "fastqc /data/*.fastq.gz"
